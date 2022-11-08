70046a20b477c196d6d2e07b3859616dade40fa1
Ravil <lllx2@yandex.ru>
2022-11-08 17:21:34 +0300

    1

---
+++ Task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найти произведение двух матриц
+int [,] firstMatrix = new int [3,3];
+int [,] secondMatrix = new int [3,3];
+int [,] result = new int [3,3];
+FillArr(firstMatrix);
+PrintArr(firstMatrix);
+FillArr(secondMatrix);
+PrintArr(secondMatrix);
+
+for(int i = 0; i<firstMatrix.GetLength(0); i++) {
+    for(int j = 0; j<secondMatrix.GetLength(1); j++) {
+        for(int k = 0; k<secondMatrix.GetLength(0); k++) {
+            result[i,j] += firstMatrix [i,k] * secondMatrix[k,j];
+        }
+        Console.Write(result[i,j] + ", ");
+    }
+    Console.WriteLine();
+}
+
+void FillArr (int [,] num) {
+    for(int i =0; i<num.GetLength(0); i++) {
+        for(int j =0; j<num.GetLength(1); j++) {
+            num [i, j] = new Random().Next(1,10);
+
+        }
+    }
+}
+
+void PrintArr(int [,] num) {
+    for(int i =0; i<num.GetLength(0); i++) {
+        for(int j =0; j<num.GetLength(1); j++) {
+            Console.Write($"{num [i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
